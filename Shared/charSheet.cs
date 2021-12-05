748f93bbb5054a094ddbecd66b22e1746ce05c02
Honk <throwaway0000ff@gmail.com>
2021-12-05 00:36:46 -0700

    Adds boxes for powers, equipment, advantages, and skills

--- Shared/charSheet.cs
+++ Shared/charSheet.cs
@@ -9,5 +9,10 @@ namespace honksite.Shared
         public string name = "Name";
         public string pronouns = "Pronouns";
         public string imageUrl = "https://art.ngfiles.com/images/2110000/2110060_sabtastic_kenku-wizard-commission.png?f1633212952";
+        public string powers = "";
+        public string equipment = "";
+        public string advantages = "";
+        public string skills = "";
+        public string complications = "";
     }
 }
