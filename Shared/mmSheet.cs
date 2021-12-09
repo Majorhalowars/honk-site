8493ad72a26ba5dcbcd328cdf0c6459f7608ba77
Honk <throwaway0000ff@gmail.com>
2021-12-08 22:51:58 -0700

    Try 2

--- Shared/mmSheet.cs
+++ Shared/mmSheet.cs
@@ -10,9 +10,9 @@ namespace honksite.Shared
         public string powerLevel = "PL";
         public string imageUrl = "https://art.ngfiles.com/images/2110000/2110060_sabtastic_kenku-wizard-commission.png?f1633212952";
         public List<string> powers = new();
-        public string equipment = "";
-        public string advantages = "";
-        public string skills = "";
+        public List<string> equipment = new();
+        public List<string> advantages = new();
+        public List<string> skills = new();
         public string complications = "";
         public Dictionary<string, int> stats = new() { { "Strength", 0 }, { "Agilty", 0 }, { "Fighting", 0 }, { "Awareness", 0 }, { "Stamina", 0 }, { "Dexterity", 0 }, { "Intellect", 0 }, { "Presence", 0 } };
     }
