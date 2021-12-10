1811d34db806faaebb3b8aef2865308271dcad5b
Honk <throwaway0000ff@gmail.com>
2021-12-09 18:07:16 -0700

    The search for the missing grouping symbol woo

--- Shared/mmSheet.cs
+++ Shared/mmSheet.cs
@@ -15,8 +15,7 @@ namespace honksite.Shared
         public List<string> skills = new();
         public string complications = "";
         public Dictionary<string, int> stats = new() { { "Strength", 0 }, { "Agilty", 0 }, { "Fighting", 0 }, { "Awareness", 0 }, { "Stamina", 0 }, { "Dexterity", 0 }, { "Intellect", 0 }, { "Presence", 0 } };
-        public Dictionary<string, int> spentPoints = new() { { "Abilities", 0 }, { "Skills", 0 }, { "Powers", 0 }, { "Defenses", 0 }, { "Advantages", 0 } };
-        public Dictionary<string, int> defense = new() { { "Dodge", 0 } };
+        public Dictionary<string, int> defenses = new() { { "Dodge", 0 } };
         
         }
 }
