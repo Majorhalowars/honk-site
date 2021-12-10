03e0863db920805c319915aa40b58f70b24289cf
Honk <throwaway0000ff@gmail.com>
2021-12-09 20:55:41 -0700

    more typos

--- Shared/mmSheet.cs
+++ Shared/mmSheet.cs
@@ -7,15 +7,15 @@ namespace honksite.Shared
     public class mmSheet
     {
         public string name = "Name";
-        public int powerLevel = "PL";
+        public string powerLevel = "PL";
         public string imageUrl = "https://art.ngfiles.com/images/2110000/2110060_sabtastic_kenku-wizard-commission.png?f1633212952";
         public List<string> powers = new();
         public List<string> equipment = new();
         public List<string> advantages = new();
         public List<string> skills = new();
         public string complications = "";
-        public int initiative = "0";
-        public List<string> offense = "";
+        public string initiative = "0";
+        public List<string> offense = new()"";
         public Dictionary<string, int> stats = new() { { "Strength", 0 }, { "Agilty", 0 }, { "Fighting", 0 }, { "Awareness", 0 }, { "Stamina", 0 }, { "Dexterity", 0 }, { "Intellect", 0 }, { "Presence", 0 } };
         public Dictionary<string, int> defenses = new() { { "Dodge", 0 }, { "Fortitude", 0}, { "Parry", 0}, { "Toughness", 0}, { "Will", 0} };
         public Dictionary<string, int> spentPoints = new() { { "Abilities", 0 }, { "Skills", 0 }, { "Powers", 0 }, { "Defenses", 0 }, { "Advantages", 0 } };
