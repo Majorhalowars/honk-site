94b3e593b9c02bc4c3f85e60ea504253e6f7e552
Honk <throwaway0000ff@gmail.com>
2022-06-30 15:53:47 -0600

    attempt 2 to add matblazor

---
+++ Shared/fateSheet.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace honksite.Shared;
+
+    public class fateSheet
+{
+    public string aspectName = "Aspect";
+    public string aspectDescription = "Aspect Details";
+}
