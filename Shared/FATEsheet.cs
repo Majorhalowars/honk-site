3e8afec03efe5392339e7d9f96c167f38e4faec5
Honk <throwaway0000ff@gmail.com>
2022-07-01 10:39:09 -0600

    First attempt at dyanmic aspect cards

--- Shared/FATEsheet.cs
+++ Shared/FATEsheet.cs
@@ -6,6 +6,16 @@ namespace honksite.Shared;
 
     public class FATEsheet
     {
-        public string aspectName = "";
-        public string aspectDescription = "";
+        public string name = "";
+        public string description = "";
+        public Dictionary<string, skill> skills = new();
+
+        public class aspect : IEquatable<Part>
+        {
+            public string aspectName { get; set;}
+            public string aspectDescription { get; set;}
+        }
+        public List<aspect> aspectList = new() { { aspectName = "First Aspect", aspectDescription = "High Concept" },{ aspectName = "Second Aspect", aspectDescription = "Trouble/Complication" }};
     }
+    
+    
