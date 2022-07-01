9d4853000d7d505bd7717ebbea1873a2b218ef31
Honk <throwaway0000ff@gmail.com>
2022-07-01 15:06:55 -0600

    thiiis maybe

--- Shared/FATEsheet.cs
+++ Shared/FATEsheet.cs
@@ -10,15 +10,11 @@ namespace honksite.Shared;
         public string description = "";
         public Dictionary<string, int> skills = new();
         
-        public List<aspectclass> aspectList = new() {{"High Concept", "funny words here"}};
+        public List<aspectclass> aspectList = new();
+        
     }
     public class aspectclass
         {
             public string aspectName {get; set;}
             public string aspectDescription {get; set;}
-
-            public override string ToString()
-            {
-                return "name: " + aspectName + " desc: " + aspectDescription;
-            }
         }
