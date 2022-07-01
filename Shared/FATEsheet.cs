9f899df6ac99f188b657231f0b22aae8ded94acd
Honk <throwaway0000ff@gmail.com>
2022-07-01 11:04:21 -0600

    Friendship with tuples intitated

--- Shared/FATEsheet.cs
+++ Shared/FATEsheet.cs
@@ -10,29 +10,7 @@ namespace honksite.Shared;
         public string description = "";
         public Dictionary<string, int> skills = new();
 
-        public class aspect : IEquatable<aspect>
-        {
-            public string aspectName { get; set;}
-            public string aspectDescription { get; set;}
-
-            public override bool Equals(object obj)
-            {
-                if (obj == null) return false;
-                aspect objAsAspect = obj as aspect;
-                if (objAsAspect == null) return false;
-                else return Equals(objAsAspect);
-            }
-            public override int GetHashCode()
-            {
-                return aspectName;
-            }
-            public bool Equals(aspect other)
-            {
-                if (other == null) return false;
-                return (this.aspectName.Equals(other.aspectName));
-            }
-        }
-        public List<aspect> aspectList = new() { { aspectName = "First Aspect", aspectDescription = "High Concept" },{ aspectName = "Second Aspect", aspectDescription = "Trouble/Complication" }};
+        public List<Tuple<string, string>> aspectList = new() { {"First Aspect","High Concept" },{"Second Aspect","Trouble/Complication" }};
     }
     
     
