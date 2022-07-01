d06ce232b04f7a4e582f4b0b7fa5feede6df42e2
Honk <throwaway0000ff@gmail.com>
2022-07-01 10:48:03 -0600

    removes the button and cleans some more bugs

--- Shared/FATEsheet.cs
+++ Shared/FATEsheet.cs
@@ -8,12 +8,29 @@ namespace honksite.Shared;
     {
         public string name = "";
         public string description = "";
-        public Dictionary<string, skill> skills = new();
+        public Dictionary<string, int> skills = new();
 
-        public class aspect : IEquatable<Part>
+        public class aspect : IEquatable<aspect>
         {
             public string aspectName { get; set;}
             public string aspectDescription { get; set;}
+
+            public override bool Equals(object obj)
+            {
+                if (obj == null) return false;
+                aspect objAsAspect = obj as aspect;
+                if (objAsAspect == null) return false;
+                else return Equals(objAsAspect);
+            }
+            public override int GetHashCode()
+            {
+                return aspectName;
+            }
+            public bool Equals(Part other)
+            {
+                if (other == null) return false;
+                return (this.aspectName.Equals(other.aspectName));
+            }
         }
         public List<aspect> aspectList = new() { { aspectName = "First Aspect", aspectDescription = "High Concept" },{ aspectName = "Second Aspect", aspectDescription = "Trouble/Complication" }};
     }
